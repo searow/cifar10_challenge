cfa5cf8988bdc2b2aafc89931c0ee194ded85de6
Chris Ro <christopher.j.ro@gmail.com>
2018-12-02 13:20:44 -0800

    Add defaults for cifar

--- parametric_attacks.sh
+++ parametric_attacks.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-ks=( 16 32 64 128 256 )
-top_grads=( 1 2 4 8 16 32 64 )
-as=( 0.01 0.02 0.04 0.08 0.16 0.32 )
+ks=( 20 )
+top_grads=( 64 128 256 512 )
+as=( 2 )
 
 for k in ${ks[@]}; do
   for grad in ${top_grads[@]}; do
